5ed30d4d3cbb15bf5f60f73383b46836c475960c
Nguyen Quoc Thang <nguyenquocthang0212@gmail.com>
2025-02-06 15:55:11 +0700

    Update: Fix the protocol communicate -> data not to be fragmented

--- utils/communication.py
+++ utils/communication.py
@@ -1,14 +1,24 @@
-import pickle 
-import sys
+import struct
+import pickle
 
 def send_message(conn, msg):
-    conn.send(pickle.dumps(f"{sys.getsizeof(pickle.dumps(msg))}"))
-    conn.sendall(pickle.dumps(msg))
+    serialized_msg = pickle.dumps(msg)
+    msg_length = struct.pack('!I', len(serialized_msg))  # Đóng gói kích thước (4 byte)
+    conn.sendall(msg_length + serialized_msg)  # Gửi kích thước trước, sau đó dữ liệu
 
 def receive_message(conn):
-    received_data = pickle.loads(conn.recv(1024))
-    mgs=b''
-    while not(mgs):
-        mgs=conn.recv(int(received_data))
-    res = pickle.loads(mgs)
-    return res
+    # Nhận đúng 4 byte đầu tiên (chứa kích thước dữ liệu)
+    msg_length_data = conn.recv(4)
+    if not msg_length_data:
+        return None
+    msg_length = struct.unpack('!I', msg_length_data)[0]  # Giải mã kích thước
+
+    # Nhận toàn bộ dữ liệu
+    received_data = b""
+    while len(received_data) < msg_length:
+        packet = conn.recv(msg_length - len(received_data))  # Nhận phần còn thiếu
+        if not packet:
+            return None
+        received_data += packet
+
+    return pickle.loads(received_data)  # Giải mã object
